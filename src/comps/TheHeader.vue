1837f7656e34a74eb17dac416397afffb308263c
Kamedin <dev@dev.dev>
2024-02-10 13:04:52 +0100

    added assets and header

---
+++ src/comps/TheHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <header class="d-flex flex-column justify-content-center align-items-start">
+        <div class="d-flex justify-content-center align-items-center">
+            <h1>MENTAL MATH</h1>
+        </div>
+        <div class="d-flex justify-content-center align-items-center">
+            <h4>TRAIN YOUR BRAIN!</h4>
+        </div>
+    </header>
+</template>
+
+
+
+<style scoped>
+header {
+    height: 210px;
+    background-image: url("@/assets/img/header_backgrounds/header_800.png");
+    background-size: 100% 100%;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+// ASSETS
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+// FONTAWESOME
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+
+
+const app = createApp(App);
+app.component("fa-icon", FontAwesomeIcon);
+app.mount('#app')
